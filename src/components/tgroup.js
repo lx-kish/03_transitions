f2f71d6692a6e6d570f97c55787c7131aba247ce
lx kish <urban.ktulhu@gmail.com>
2018-06-29 07:36:18 +1200

    39 Transition Group

--- src/components/tgroup.js
+++ src/components/tgroup.js
@@ -1,41 +1,60 @@
 import React, { Component } from 'react';
-import '../css/App.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-class Slide extends Component{
+import '../css/App.css';
+import TransitionComp from './Transition';
+
+class Slide extends Component {
     state = {
-        items:[]
+        items: []
     }
-    
-    addElements () {
-        return this.state.items.map((item,i) => (
-            <div className="item" key={i}>{item}</div>
+
+    addElements() {
+        return this.state.items.map((item, i) => (
+            <CSSTransition
+                classNames="item"
+                timeout={500}
+                key={i}
+                onEntered={ (node) =>{
+                    node.classList.add("active")
+                }}
+            >
+                <div className="item" key={i}>{item}</div>
+            </CSSTransition>
+
         ));
     }
 
-    generateNumber(){
+    generateNumber() {
         //let random = Math.floor(Math.random() * 100) + 1;
-        let newArray = [...this.state.items,Math.floor(Math.random() * 100) + 1]
+        let newArray = [...this.state.items, Math.floor(Math.random() * 100) + 1]
 
         this.setState({
-            items:newArray
+            items: newArray
         })
     }
 
-    removeNumber(){
-        let newArray = this.state.items.slice(0,-1);
+    removeNumber() {
+        let newArray = this.state.items.slice(0, -1);
         this.setState({
-            items:newArray
+            items: newArray
         })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                {this.addElements()}
-             
+                <TransitionGroup
+                    component="div"
+                    className="list"
+                >
+                    {this.addElements()}
+                </TransitionGroup>
+
+
                 <div className="btns">
-                    <div className="btn-add" onClick={()=> this.generateNumber()}>Add Elements</div>
-                    <div className="btn-remove" onClick={()=> this.removeNumber()}>Remove Elements</div>
+                    <div className="btn-add" onClick={() => this.generateNumber()}>Add Elements</div>
+                    <div className="btn-remove" onClick={() => this.removeNumber()}>Remove Elements</div>
                 </div>
             </div>
         )

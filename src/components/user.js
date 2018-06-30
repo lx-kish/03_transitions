993e43ebb77af08b2c40214a6428645700e118df
lx kish <urban.ktulhu@gmail.com>
2018-06-30 15:12:24 +1200

    42 React Refs

--- src/components/user.js
+++ src/components/user.js
@@ -10,12 +10,25 @@ class User extends Component {
         spanish:false,
         message(){console.log('hey')},
         car:{brand:"Ford",model:"Focus"},
-        mother:'Martha'
+        mother:'Martha',
+        color:'red'
+    }
+
+    changeColor(){
+        // this.setState({
+        //     color:'blue'
+        // })
+        this.refs.myColor.style.color = 'blue'
     }
 
     render() {
+        const style = {
+            color: this.state.color
+        }
         return (
             <div>
+                <h4 style={style} ref="myColor">{this.state.mother}</h4>
+                <div onClick={()=>this.changeColor()}>Change color</div>
                 <UserTemplate {...this.state}/>
             </div>
         );
